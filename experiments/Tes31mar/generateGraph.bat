a4a13bf6436886a03b0316b5bfafe16ce33f1dbd
arleserp <arles.rodriguez@gmail.com>
2016-03-31 19:17:08 -0500

    Graph statistics alpha version.

---
+++ experiments/Tes31mar/generateGraph.bat
@@ -0,0 +1,13 @@
+java  -Xmx4200m -classpath NetworkSimulator.jar unalcol.agents.NetworkSim.graphGenerator smallworld 100 0.1 4
+java  -Xmx4200m -classpath NetworkSimulator.jar unalcol.agents.NetworkSim.graphGenerator smallworld 100 0.3 4
+java  -Xmx4200m -classpath NetworkSimulator.jar unalcol.agents.NetworkSim.graphGenerator smallworld 100 0.5 4
+java  -Xmx4200m -classpath NetworkSimulator.jar unalcol.agents.NetworkSim.graphGenerator smallworld 100 0.9 4
+java  -Xmx4200m -classpath NetworkSimulator.jar unalcol.agents.NetworkSim.graphGenerator lattice 10 10
+java  -Xmx4200m -classpath NetworkSimulator.jar unalcol.agents.NetworkSim.graphGenerator community 100 0.1 4 4
+java  -Xmx4200m -classpath NetworkSimulator.jar unalcol.agents.NetworkSim.graphGenerator community 100 0.3 4 4
+java  -Xmx4200m -classpath NetworkSimulator.jar unalcol.agents.NetworkSim.graphGenerator community 100 0.5 4 4
+java  -Xmx4200m -classpath NetworkSimulator.jar unalcol.agents.NetworkSim.graphGenerator community 100 0.9 4 4
+java  -Xmx4200m -classpath NetworkSimulator.jar unalcol.agents.NetworkSim.graphGenerator scalefree 4 4 97
+java  -Xmx4200m -classpath NetworkSimulator.jar unalcol.agents.NetworkSim.graphGenerator scalefree 4 2 97
+rem java  -Xmx4200m -classpath NetworkSimulator.jar unalcol.agents.NetworkSim.graphGenerator scalefree 4 1 97
+pause
