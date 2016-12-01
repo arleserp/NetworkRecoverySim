b63c83e7a0f8af34e76cca400d17fade22459ca7
arlese.rodriguezp <arles.rodriguez@gmail.com>
2016-11-30 21:26:28 -0500

    Added evaporation for carriersrep algorithm for avoiding stagnation on scale-free networks.

--- experiments/20161129ReplicationScaleFree/runall.bat
+++ experiments/20161129ReplicationScaleFree/runall.bat
@@ -1,6 +1,6 @@
 for /L %%x in (1, 1, 30) do (
   echo %%x%round
-  java   -classpath dist/NetworkSimulator.jar unalcol.agents.NetworkSim.DataReplicationMain load scalefree+sn+4+eta+1+numSt+97.graph 10 0  carriersrep scalefree+sn+4+eta+1+numSt+97.loc -1 
+  java   -classpath dist/NetworkSimulator.jar unalcol.agents.NetworkSim.DataReplicationMain load scalefree+sn+4+eta+1+numSt+97.graph 10 0.5  carriersrep scalefree+sn+4+eta+1+numSt+97.loc -1 
   rem java   -classpath dist/NetworkSimulator.jar unalcol.agents.NetworkSim.DataReplicationMain load scalefree+sn+4+eta+1+numSt+97.graph 10 0.05  carriersrep scalefree+sn+4+eta+1+numSt+97.loc -1 
   rem java   -classpath dist/NetworkSimulator.jar unalcol.agents.NetworkSim.DataReplicationMain load scalefree+sn+4+eta+1+numSt+97.graph 10 0.1  carriersrep scalefree+sn+4+eta+1+numSt+97.loc -1 
   rem java   -classpath dist/NetworkSimulator.jar unalcol.agents.NetworkSim.DataReplicationMain load scalefree+sn+4+eta+1+numSt+97.graph 10 0.3  carriersrep scalefree+sn+4+eta+1+numSt+97.loc -1 
