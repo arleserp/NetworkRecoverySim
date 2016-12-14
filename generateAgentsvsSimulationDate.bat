d0dff602889b242fb44b00afdf6984085099b2e1
ARODRIGUEZ <ARODRIGUEZ@ARODRIGUEZ-PC>
2016-12-13 22:28:14 -0500

    Addition of statistics regarding memory consumption and messages sent and received.

--- generateAgentsvsSimulationDate.bat
+++ generateAgentsvsSimulationDate.bat
@@ -1,1 +1,3 @@
-java  -Xmx4200m -classpath dist/NetworkSimulator.jar unalcol.agents.NetworkSim.util.GrowthCurveAgentVsSimulationNumber 
+java  -Xmx4200m -classpath dist/NetworkSimulator.jar unalcol.agents.NetworkSim.util.GrowthCurveAgentVsSimulationNumber
+java  -Xmx4200m -classpath dist/NetworkSimulator.jar unalcol.agents.NetworkSim.util.ReplicationTaskSuccessReport
+java  -Xmx4200m -classpath dist/NetworkSimulator.jar unalcol.agents.NetworkSim.util.BoxPlotMemoryConsumptionReplication 
