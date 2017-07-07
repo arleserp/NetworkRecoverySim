f321fea50ef7bd81fc54521bfc07ad92e795810c
arlese.rodriguezp <arles.rodriguez@gmail.com>
2017-07-07 16:53:30 -0500

    Experiments and script to run simulation.

--- runSimulation.bat
+++ runSimulation.bat
@@ -1,3 +1,3 @@
-for /L %%x in (1, 1, 27) do (
+for /L %%x in (1, 1, 30) do (
 java  -d64   -Xms4G -Xmx4G  -XX:+UseG1GC  -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:ParallelGCThreads=20 -XX:ConcGCThreads=5 -XX:InitiatingHeapOccupancyPercent=70  -classpath dist/NetworkSimulator.jar unalcol.agents.NetworkSim.DataReplicationNodeFailingMain load smallworld+v+100+beta+0.5+degree+2.graph 20 0 carriersrep smallworld+v+100+beta+0.5+degree+2.loc 5000 replalgon NODELAY 0.001 6 chain 4
 )
