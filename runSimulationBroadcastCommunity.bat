d8d558c7557ed8f573e067a1d3deacd5059ff4c1
Arles <Arles@LAPTOP-A13EVLJ9>
2017-07-12 07:22:32 -0500

    community experiments.

---
+++ runSimulationBroadcastCommunity.bat
@@ -0,0 +1,3 @@
+for /L %%x in (1, 1, 30) do (
+java  -d64   -Xms4G -Xmx4G  -XX:+UseG1GC  -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:ParallelGCThreads=20 -XX:ConcGCThreads=5 -XX:InitiatingHeapOccupancyPercent=70  -classpath dist/NetworkSimulator.jar unalcol.agents.NetworkSim.DataReplicationNodeFailingMain load communitycircle+v+100+beta+0.5+degree+4+clusters+4.graph 20 0 carriersrep communitycircle+v+100+beta+0.5+degree+4+clusters+4.loc 5000 replalgon NODELAY 0.001 6 broadcast
+)
